cbaca5da049fa182af708e8762f206e37c90b51c
Alfonso <plutoyem@outlook.com>
2019-04-14 15:12:26 -0400

    ind

--- advancedSearch.py
+++ advancedSearch.py
@@ -4,10 +4,10 @@ import rankWorker
 
 
 def advancedSearch(query:str):
-	q = queryWorker.QueryWorker()
-	words, index2docs = q.output(query)
-	m = mongodbWorker.MongodbWorker()
-	r = rankWorker.RankWorker(m)
-	r.input(words, index2docs)
+    q = queryWorker.QueryWorker()
+    words, index2docs = q.output(query)
+    m = mongodbWorker.MongodbWorker()
+    r = rankWorker.RankWorker(m)
+    r.input(words, index2docs)
     docs = r.output()
     return docs
