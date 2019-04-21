d6c2b6571e83f0ed454c74dbef8f6e2875ef4cd0
Hao Yin <yinhao0424@gmail.com>
2019-04-20 22:08:53 -0400

    add title

--- search_inverted.py
+++ search_inverted.py
@@ -45,13 +45,33 @@ class Search(Index):
 
         return results
 
-def main(args):
-    query = ["fashion","show"]
-    results = Search().search_inverted(query)
-    results1 = Search().search_script(query)
+    def search_title(self,query):
+        results = {}
 
-    print(results)
-    print(results1)
+        client = pymongo.MongoClient("mongodb://jack:jackmongodb@cluster0-shard-00-00-uagde.mongodb.net:27017,cluster0-shard-00-01-uagde.mongodb.net:27017,cluster0-shard-00-02-uagde.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true")
+        db = client['IMDBData']
+        collection = db['Movies_6']
+
+        for word in query:
+            tokens = self.tokenize(word)
+            stems = self.stemming(tokens)
+
+            query = {stems[0]: {"$exists": True}}
+            cursor = collection.find(query,{"_id":0})
+
+            for i in cursor:
+                results.update(i)
+
+        return results
+
+def main(args):
+    query = ["summer"]
+    # results = Search().search_inverted(query)
+    # results1 = Search().search_script(query)
+    results2 = Search().search_title(query)
+    print(results2)
+    # print(results)
+    # print(results1)
 
 
 if __name__ == "__main__":
